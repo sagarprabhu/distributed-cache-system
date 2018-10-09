14bafe16b98d35bb8b55ccb568c53f19652630bc
siddheshgupte <siddhesh.gupte2468@gmail.com>
2018-10-09 06:04:58 +0530

    Add successors and predecessors

--- lib/application1.ex
+++ lib/application1.ex
@@ -1,31 +1,84 @@
 defmodule Application1 do
-    use Application
-    @m 100
-    def main(args \\ []) do
-        
-        Application1.start(:abc, String.to_integer(Enum.at(args, 0)), String.to_integer(Enum.at(args, 1)))
+  use Application
+  @m 10
+  def main(args \\ []) do
+    Application1.start(
+      :abc,
+      String.to_integer(Enum.at(args, 0)),
+      String.to_integer(Enum.at(args, 1))
+    )
 
-        # receive do
-        #     {:hi, message} -> IO.puts message
-        # end
-    end
+    # receive do
+    #     {:hi, message} -> IO.puts message
+    # end
+  end
 
-    def start(_type, num_of_nodes, num_of_messages) do
+  def start(_type, num_of_nodes, num_of_messages) do
     children =
       1..num_of_nodes
       |> Enum.to_list()
       |> Enum.map(fn x ->
-        {sliced_hash, _} = :crypto.hash(:sha, Integer.to_string(x)) |> Base.encode16 |> String.slice(0, @m) |> Integer.parse(16)
+        {decimal_hash, _} =
+          :crypto.hash(:sha, Integer.to_string(x)) |> Base.encode16() |> Integer.parse(16)
 
-        IO.inspect sliced_hash
-        
+        sliced_hash =
+          decimal_hash |> Integer.to_string(2) |> String.slice(0, @m) |> String.to_integer(2)
+
         Supervisor.child_spec(
-          {DosProj3, [String.to_atom("N#{sliced_hash}")]},
-          id: String.to_atom("N#{sliced_hash}")
+          {DosProj3, [String.to_atom("#{sliced_hash}")]},
+          id: String.to_atom("#{sliced_hash}")
         )
       end)
-     opts = [strategy: :one_for_one, name: Supervisor]
+
+    opts = [strategy: :one_for_one, name: Supervisor]
     {:ok, supervisor} = Supervisor.start_link(children, opts)
-     Supervisor.which_children(supervisor)
+    
+    lst = 
+      Supervisor.which_children(supervisor)
+        |> Enum.map(fn x -> elem(x, 0) end)
+        |> Enum.map(fn x -> Atom.to_string(x) end)
+        |> Enum.map(fn x -> String.to_integer(x) end)
+        |> Enum.sort
+        |> Enum.map(fn x -> Integer.to_string(x) end)
+        |> Enum.map(fn x -> String.to_atom(x) end)
+    
+    # list to hold node values for find_successor, closest_preceding_node functions
+    node_values =
+      lst
+      |> Enum.map(fn x -> Atom.to_string(x) end)
+      |> Enum.map(fn x -> String.to_integer(x) end)  
+      |> IO.inspect
+      
+    r = trunc(:math.log2(num_of_nodes)) 
+
+    0..(num_of_nodes - 1)
+      |> Enum.to_list
+      |> Enum.each(fn x -> 
+
+        successors =
+          1..r
+            |> Enum.to_list()
+            |> Enum.map(
+              fn curr_r ->
+                Enum.at(lst, rem(x + curr_r, num_of_nodes))
+              end    
+            )
+
+        GenServer.cast(
+          Enum.at(lst, x),
+          {:set_neighbours, [Enum.at(lst, x-1)] ++ successors}
+        )
+      end)
+    
+    # 0..(num_of_nodes - 1)
+    #   |> Enum.to_list
+    #   |> Enum.each(
+    #     fn x ->
+    #       finger_table =
+    #         1..r
+    #           |> Enum.to_list
+    #           |> Enum.map(find)
+    #     end
+    #   )
   end
 end
--- lib/dos_proj3.ex
+++ lib/dos_proj3.ex
@@ -1,14 +1,11 @@
 defmodule DosProj3 do
- use GenServer
+  use GenServer
 
   # External API
   def start_link([input_name]) do
     GenServer.start_link(
       __MODULE__,
-      %{:finger_table => [],
-        :predecessor => [],
-        :successor => [],
-        :local_file => []},
+      %{:finger_table => [], :predecessor => [], :successor => [], :local_file => []},
       name: input_name
     )
   end
@@ -19,13 +16,41 @@ defmodule DosProj3 do
   end
 
   def handle_call(:gossip, _from, current_map) do
-    {_, updated_map} = Map.get_and_update(current_map, :current_gossip_count, fn x -> {x, x - 1} end)
+    {_, updated_map} =
+      Map.get_and_update(current_map, :current_gossip_count, fn x -> {x, x - 1} end)
+
     {:reply, current_map, updated_map}
   end
 
   def handle_cast(:gossip, current_map) do
-    {_, updated_map} = Map.get_and_update(current_map, :current_gossip_count, fn x -> {x, x - 1} end)
-    IO.puts current_map.current_gossip_count
+    {_, updated_map} =
+      Map.get_and_update(current_map, :current_gossip_count, fn x -> {x, x - 1} end)
+
+    IO.puts(current_map.current_gossip_count)
     {:noreply, updated_map}
   end
+
+  def handle_cast({:set_neighbours, list_neighbours}, current_map) do
+
+    [ input_predecessor | input_successors ] = list_neighbours
+
+    {_, updated_map} =
+      Map.get_and_update(current_map, :predecessor, fn x -> {x, x ++ input_predecessor} end)
+  
+    {_, updated_map} =
+    Map.get_and_update(updated_map, :successor, fn x -> {x, x ++ input_successors} end)
+
+    IO.inspect updated_map
+    {:noreply, updated_map}
+  end
+
+  def handle_cast({:set_finger_table, finger_table}, current_map) do
+
+    {_, updated_map} =
+      Map.get_and_update(current_map, :finger_table, fn x -> {x, x ++ finger_table} end)
+  
+    IO.inspect updated_map
+    {:noreply, updated_map}
+  end
+
 end
