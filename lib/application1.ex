c3445ffbfe3913c6defa9d106ea53ad8839043de
siddheshgupte <siddhesh.gupte2468@gmail.com>
2018-10-11 21:25:47 +0530

    Add search base

--- lib/application1.ex
+++ lib/application1.ex
@@ -4,13 +4,13 @@ defmodule Application1 do
   @m 10
 
   def get_sliced_hash(val) do
-
     {decimal_hash, _} =
       if is_integer(val) do
         :crypto.hash(:sha, Integer.to_string(val)) |> Base.encode16() |> Integer.parse(16)
       else
         :crypto.hash(:sha, val) |> Base.encode16() |> Integer.parse(16)
       end
+
     sliced_hash =
       decimal_hash |> Integer.to_string(2) |> String.slice(0, @m) |> String.to_integer(2)
   end
@@ -19,7 +19,7 @@ defmodule Application1 do
     if n in node_values do
       n
     else
-      find_finger(rem((n+1), trunc(:math.pow(2, @m))), node_values)
+      find_finger(rem(n + 1, trunc(:math.pow(2, @m))), node_values)
     end
   end
 
@@ -40,9 +40,8 @@ defmodule Application1 do
       1..num_of_nodes
       |> Enum.to_list()
       |> Enum.map(fn x ->
-
         sliced_hash = get_sliced_hash(x)
-        
+
         Supervisor.child_spec(
           {DosProj3, [String.to_atom("#{sliced_hash}")]},
           id: String.to_atom("#{sliced_hash}")
@@ -51,80 +50,82 @@ defmodule Application1 do
 
     opts = [strategy: :one_for_one, name: Supervisor]
     {:ok, supervisor} = Supervisor.start_link(children, opts)
-    
-    lst = 
+
+    lst =
       Supervisor.which_children(supervisor)
-        |> Enum.map(fn x -> elem(x, 0) end)
-        |> Enum.map(fn x -> Atom.to_string(x) end)
-        |> Enum.map(fn x -> String.to_integer(x) end)
-        |> Enum.sort
-        |> Enum.map(fn x -> Integer.to_string(x) end)
-        |> Enum.map(fn x -> String.to_atom(x) end)
-    
+      |> Enum.map(fn x -> elem(x, 0) end)
+      |> Enum.map(fn x -> Atom.to_string(x) end)
+      |> Enum.map(fn x -> String.to_integer(x) end)
+      |> Enum.sort()
+      |> Enum.map(fn x -> Integer.to_string(x) end)
+      |> Enum.map(fn x -> String.to_atom(x) end)
+
     # list to hold node values for find_successor, closest_preceding_node functions
     node_values =
       lst
       |> Enum.map(fn x -> Atom.to_string(x) end)
-      |> Enum.map(fn x -> String.to_integer(x) end)  
-      |> IO.inspect
-      
-    r = trunc(:math.log2(num_of_nodes)) * 2 
+      |> Enum.map(fn x -> String.to_integer(x) end)
+      |> IO.inspect()
+
+    r = trunc(:math.log2(num_of_nodes)) * 2
 
     0..(num_of_nodes - 1)
-      |> Enum.to_list
-      |> Enum.each(fn x -> 
+    |> Enum.to_list()
+    |> Enum.each(fn x ->
+      successors =
+        1..r
+        |> Enum.to_list()
+        |> Enum.map(fn curr_r ->
+          Enum.at(lst, rem(x + curr_r, num_of_nodes))
+        end)
+        |> Enum.map(fn x -> Atom.to_string(x) end)
+        |> Enum.map(fn x -> String.to_integer(x) end)
 
-        successors =
-          1..r
-            |> Enum.to_list()
-            |> Enum.map(
-              fn curr_r ->
-                Enum.at(lst, rem(x + curr_r, num_of_nodes))
-              end)
-            |> Enum.map(fn x -> Atom.to_string(x) end)
-            |> Enum.map(fn x -> String.to_integer(x) end)
-
-        GenServer.cast(
-          Enum.at(lst, x),
-          {:set_neighbours, [Enum.at(lst, x-1) |> Atom.to_string |> String.to_integer] ++ successors}
-        )
-      end)
-    
-    0..(num_of_nodes - 1)
-      |> Enum.to_list
-      |> Enum.map(
-        fn x ->
-          finger_table =
-            0..@m-1
-              |> Enum.to_list
-              |> Enum.map(fn i ->
-                value_to_find = rem((Enum.at(node_values, x) + trunc(:math.pow(2, i))), trunc(:math.pow(2, @m)))
-                find_finger(value_to_find , node_values)
-              end
-              )
-          GenServer.cast(Enum.at(lst, x), {:set_finger_table, finger_table})
-        end
+      GenServer.cast(
+        Enum.at(lst, x),
+        {:set_neighbours,
+         [Enum.at(lst, x - 1) |> Atom.to_string() |> String.to_integer()] ++ successors}
       )
-    
-      1..num_of_messages
-        |> Enum.to_list
-        |> Enum.each(fn x -> make_file([Integer.to_string(x) <> ".mp3"], node_values) end)
-      
+    end)
+
+    0..(num_of_nodes - 1)
+    |> Enum.to_list()
+    |> Enum.map(fn x ->
+      finger_table =
+        0..(@m - 1)
+        |> Enum.to_list()
+        |> Enum.map(fn i ->
+          # value_to_find = rem((Enum.at(node_values, x) + trunc(:math.pow(2, i))), trunc(:math.pow(2, @m)))
+          value_to_find = rem(Enum.at(node_values, x) + (1 <<< i), 1 <<< @m)
+          find_finger(value_to_find, node_values)
+        end)
+
+      GenServer.cast(Enum.at(lst, x), {:set_finger_table, finger_table})
+    end)
+
+    1..num_of_messages
+    |> Enum.to_list()
+    |> Enum.each(fn x -> make_file([Integer.to_string(x) <> ".mp3"], node_values) end)
+
+    make_file(["abc.mp3"], node_values)
+
     :timer.sleep(5000)
     # Check states
     lst
-      |> Enum.each(fn x -> GenServer.cast(x, {:print_state}) end)
-      
+    |> Enum.each(fn x -> GenServer.cast(x, {:print_state}) end)
+
+    # GenServer.cast(Enum.at(lst, 0),  {:search, ["abc.mp3", get_sliced_hash("abc.mp3"), 0]})
   end
 
   def make_file(file_name, node_values) do
     sliced_hash = get_sliced_hash(file_name)
 
-    value_to_find = rem(sliced_hash, trunc(:math.pow(2, @m)))
+    # value_to_find = rem(sliced_hash, trunc(:math.pow(2, @m)))
+    value_to_find = rem(sliced_hash, 1 <<< @m)
 
     node_as_integer = find_finger(value_to_find, node_values)
-    node_as_atom = node_as_integer |> Integer.to_string |> String.to_atom
-    
+    node_as_atom = node_as_integer |> Integer.to_string() |> String.to_atom()
+
     GenServer.cast(node_as_atom, {:store_file, file_name})
   end
 end
--- lib/dos_proj3.ex
+++ lib/dos_proj3.ex
@@ -31,48 +31,76 @@ defmodule DosProj3 do
   end
 
   def handle_cast({:set_neighbours, list_neighbours}, current_map) do
-
-    [ input_predecessor | input_successors ] = list_neighbours
+    [input_predecessor | input_successors] = list_neighbours
 
     {_, updated_map} =
       Map.get_and_update(current_map, :predecessor, fn x -> {x, x ++ input_predecessor} end)
-  
+
     {_, updated_map} =
-    Map.get_and_update(updated_map, :successor, fn x -> {x, x ++ input_successors} end)
+      Map.get_and_update(updated_map, :successor, fn x -> {x, x ++ input_successors} end)
 
     {:noreply, updated_map}
   end
 
   def handle_cast({:set_finger_table, finger_table}, current_map) do
-
     {_, updated_map} =
       Map.get_and_update(current_map, :finger_table, fn x -> {x, x ++ finger_table} end)
-  
+
     {:noreply, updated_map}
   end
 
   def handle_cast({:store_file, file_name}, current_map) do
     {_, updated_map} =
       Map.get_and_update(current_map, :local_file, fn x -> {x, x ++ file_name} end)
-    
-    GenServer.cast(updated_map.predecessor |> Integer.to_string |> String.to_atom,
-                          {:store_file_as_backup, file_name})
-    
-    GenServer.cast(List.first(updated_map.successor) |> Integer.to_string |> String.to_atom,
-                          {:store_file_as_backup, file_name})
-    
+
+    GenServer.cast(
+      updated_map.predecessor |> Integer.to_string() |> String.to_atom(),
+      {:store_file_as_backup, file_name}
+    )
+
+    GenServer.cast(
+      List.first(updated_map.successor) |> Integer.to_string() |> String.to_atom(),
+      {:store_file_as_backup, file_name}
+    )
+
     {:noreply, updated_map}
   end
 
   def handle_cast({:store_file_as_backup, file_name}, current_map) do
     {_, updated_map} =
       Map.get_and_update(current_map, :local_file, fn x -> {x, x ++ file_name} end)
-    
+
     {:noreply, updated_map}
   end
 
   def handle_cast({:print_state}, current_map) do
-    IO.inspect current_map
+    IO.inspect(current_map)
+    {:noreply, current_map}
+  end
+
+  def handle_cast({:search, [file_name, key, hops_taken]}, current_map) do
+
+    # IO.puts "Hops taken #{hops_taken}"
+
+    is_file_found = Enum.member?(current_map.local_file, file_name)
+
+    IO.puts is_file_found
+
+    # if not is_file_found do
+    #   node_to_send_to =
+    #     current_map.finger_table
+    #     |> Enum.filter(fn x -> x <= key end)
+    #     |> List.last()
+
+    #   node_to_send_to =
+    #     if node_to_send_to == nil, do: Enum.at(current_map.successor, 0), else: node_to_send_to
+
+      GenServer.cast(node_to_send_to |> Integer.to_string |> String.to_atom, {:search, [file_name, key, hops_taken + 1]})
+    
+    else
+      IO.puts("File found in #{hops_taken} hops")
+    end
+
     {:noreply, current_map}
   end
 end
