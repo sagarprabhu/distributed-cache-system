86bedec853d72f71b7e5975854ca7923d676d825
siddheshgupte <siddhesh.gupte2468@gmail.com>
2018-10-16 23:35:47 +0530

    Change search to handle wrap around edge cases

--- lib/dos_proj3.ex
+++ lib/dos_proj3.ex
@@ -86,17 +86,17 @@ defmodule DosProj3 do
 
     if not is_file_found do
 
-      # # Check for the wrap around
-      # if key > current_map.current_value and Enum.at(current_map.successor, 0) < current_map.current_value do
-      #   # If this is the last node and the key is more than it, send the request to the first node.
-      #   node_to_send_to = Enum.at(current_map.successor, 0)
+      # Check for the wrap around
+      if key > current_map.current_value and Enum.at(current_map.successor, 0) < current_map.current_value do
+        # If this is the last node and the key is more than it, send the request to the first node.
+        node_to_send_to = Enum.at(current_map.successor, 0)
         
-      #   GenServer.cast(
-      #     node_to_send_to |> Integer.to_string() |> String.to_atom(),
-      #     {:search, [file_name, key, hops_taken + 1]}
-      #   )
+        GenServer.cast(
+          node_to_send_to |> Integer.to_string() |> String.to_atom(),
+          {:search, [file_name, key, hops_taken + 1]}
+        )
       
-      # else
+      else
         # Send request to the closest node in the finger table which is <= key
         node_to_send_to =
           current_map.finger_table
@@ -115,7 +115,7 @@ defmodule DosProj3 do
           node_to_send_to |> Integer.to_string() |> String.to_atom(),
           {:search, [file_name, key, hops_taken + 1]}
         ) 
-      # end 
+      end 
     else
       IO.puts("File found in #{hops_taken} hops")
     end
