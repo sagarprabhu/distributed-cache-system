c30c01894f81c6c574ea9cc1fa674a90fb478225
siddheshgupte <siddhesh.gupte2468@gmail.com>
2018-10-16 19:12:57 +0530

    Fix search logic

--- lib/application1.ex
+++ lib/application1.ex
@@ -3,6 +3,8 @@ defmodule Application1 do
   use Bitwise
   @m 10
 
+  # Function to get a sliced hash value based on @m
+  # Will take both Integers and Strings and return an integer value of sliced hash
   def get_sliced_hash(val) do
     {decimal_hash, _} =
       if is_integer(val) do
@@ -10,18 +12,36 @@ defmodule Application1 do
       else
         :crypto.hash(:sha, val) |> Base.encode16() |> Integer.parse(16)
       end
-
+    # Convert to binary and then slice and then convert back to integer.
     sliced_hash =
       decimal_hash |> Integer.to_string(2) |> String.slice(0, @m) |> String.to_integer(2)
   end
 
+  # Finds an entry for the finger table based on node_values recursively
+  # Should not return own_value
   def find_finger(own_value, n, node_values) do
     if n in node_values and n != own_value do
       n
     else
       find_finger(own_value, rem(n + 1, (1 <<< @m)), node_values)
-      # find_finger(own_value, rem(n + 1, trunc(:math.pow(2, @m))), node_values)
     end
+  end
+  
+  # Function to store a file in the network
+  def make_file(file_name, node_values) do
+
+    # Get key using the same logic as for nodes
+    sliced_hash = get_sliced_hash(file_name)
+
+    # value_to_find = key % 2^m 
+    value_to_find = rem(sliced_hash, 1 <<< @m)
+
+    # Don't need to pass own_value for files
+    node_as_integer = find_finger(:garbage, value_to_find, node_values)
+    node_as_atom = node_as_integer |> Integer.to_string() |> String.to_atom()
+
+    # Store file at node
+    GenServer.cast(node_as_atom, {:store_file, file_name})
   end
 
   def main(args \\ []) do
@@ -52,6 +72,8 @@ defmodule Application1 do
     opts = [strategy: :one_for_one, name: Supervisor]
     {:ok, supervisor} = Supervisor.start_link(children, opts)
 
+    # List of all Nodes in a sorted order
+    # Need sorting to maintain order in the ring
     lst =
       Supervisor.which_children(supervisor)
       |> Enum.map(fn x -> elem(x, 0) end)
@@ -61,15 +83,17 @@ defmodule Application1 do
       |> Enum.map(fn x -> Integer.to_string(x) end)
       |> Enum.map(fn x -> String.to_atom(x) end)
 
-    # list to hold node values for find_successor, closest_preceding_node functions
+    # list to hold node values as integers for find_successor, closest_preceding_node functions
     node_values =
       lst
       |> Enum.map(fn x -> Atom.to_string(x) end)
       |> Enum.map(fn x -> String.to_integer(x) end)
       |> IO.inspect()
 
+    # Number of successors for failure handling(r) = 2 * log n 
     r = trunc(:math.log2(num_of_nodes)) * 2
 
+    # Set neighbours
     0..(num_of_nodes - 1)
     |> Enum.to_list()
     |> Enum.each(fn x ->
@@ -89,6 +113,7 @@ defmodule Application1 do
       )
     end)
 
+    # Set finger tables
     0..(num_of_nodes - 1)
     |> Enum.to_list()
     |> Enum.map(fn x ->
@@ -96,7 +121,6 @@ defmodule Application1 do
         0..(@m - 1)
         |> Enum.to_list()
         |> Enum.map(fn i ->
-          # value_to_find = rem((Enum.at(node_values, x) + trunc(:math.pow(2, i))), trunc(:math.pow(2, @m)))
           value_to_find = rem(Enum.at(node_values, x) + (1 <<< i), 1 <<< @m)
           find_finger(Enum.at(node_values, x), value_to_find, node_values)
         end)
@@ -104,29 +128,24 @@ defmodule Application1 do
       GenServer.cast(Enum.at(lst, x), {:set_finger_table, finger_table})
     end)
 
+    #  Store files from 0.mp3 to <num_of_messages>.mp3
+    #  The nodes will fetch every file once
     1..num_of_messages
     |> Enum.to_list()
     |> Enum.each(fn x -> make_file([Integer.to_string(x) <> ".mp3"], node_values) end)
 
     make_file(["abc.mp3"], node_values)
+    make_file(["test"], node_values)
 
+    # Check states for Debugging
     :timer.sleep(5000)
-    # Check states
     lst
     |> Enum.each(fn x -> GenServer.cast(x, {:print_state}) end)
 
+    # Search for file
     GenServer.cast(Enum.at(lst, 0),  {:search, ["abc.mp3", get_sliced_hash("abc.mp3"), 0]})
-  end
+    # GenServer.cast(Enum.at(lst, 0),  {:search, ["test", get_sliced_hash("test"), 0]})
 
-  def make_file(file_name, node_values) do
-    sliced_hash = get_sliced_hash(file_name)
-
-    # value_to_find = rem(sliced_hash, trunc(:math.pow(2, @m)))
-    value_to_find = rem(sliced_hash, 1 <<< @m)
-
-    node_as_integer = find_finger(:garbage, value_to_find, node_values)
-    node_as_atom = node_as_integer |> Integer.to_string() |> String.to_atom()
-
-    GenServer.cast(node_as_atom, {:store_file, file_name})
   end
 end
+ 
--- lib/dos_proj3.ex
+++ lib/dos_proj3.ex
@@ -15,21 +15,7 @@ defmodule DosProj3 do
     {:ok, initial_map}
   end
 
-  def handle_call(:gossip, _from, current_map) do
-    {_, updated_map} =
-      Map.get_and_update(current_map, :current_gossip_count, fn x -> {x, x - 1} end)
-
-    {:reply, current_map, updated_map}
-  end
-
-  def handle_cast(:gossip, current_map) do
-    {_, updated_map} =
-      Map.get_and_update(current_map, :current_gossip_count, fn x -> {x, x - 1} end)
-
-    IO.puts(current_map.current_gossip_count)
-    {:noreply, updated_map}
-  end
-
+  # Neighbours
   def handle_cast({:set_neighbours, list_neighbours}, current_map) do
     [input_predecessor | input_successors] = list_neighbours
 
@@ -42,6 +28,7 @@ defmodule DosProj3 do
     {:noreply, updated_map}
   end
 
+  # Finger table
   def handle_cast({:set_finger_table, finger_table}, current_map) do
     {_, updated_map} =
       Map.get_and_update(current_map, :finger_table, fn x -> {x, x ++ finger_table} end)
@@ -49,15 +36,18 @@ defmodule DosProj3 do
     {:noreply, updated_map}
   end
 
+  # Store file
   def handle_cast({:store_file, file_name}, current_map) do
     {_, updated_map} =
       Map.get_and_update(current_map, :local_file, fn x -> {x, x ++ file_name} end)
 
+    # Send the file to predecessor for failure handling and load balance
     GenServer.cast(
       updated_map.predecessor |> Integer.to_string() |> String.to_atom(),
       {:store_file_as_backup, file_name}
     )
 
+    # Send the file to the successor for failure handling and load balance
     GenServer.cast(
       List.first(updated_map.successor) |> Integer.to_string() |> String.to_atom(),
       {:store_file_as_backup, file_name}
@@ -66,6 +56,7 @@ defmodule DosProj3 do
     {:noreply, updated_map}
   end
 
+  # Store files in the successor and predecessors using this function rather than {:store_file, file_name}
   def handle_cast({:store_file_as_backup, file_name}, current_map) do
     {_, updated_map} =
       Map.get_and_update(current_map, :local_file, fn x -> {x, x ++ file_name} end)
@@ -73,38 +64,42 @@ defmodule DosProj3 do
     {:noreply, updated_map}
   end
 
+  # Print states for debugging
   def handle_cast({:print_state}, current_map) do
     IO.inspect(current_map)
     {:noreply, current_map}
   end
 
+  # Search for file by file_name
   def handle_cast({:search, [file_name, key, hops_taken]}, current_map) do
 
-    # IO.puts "Hops taken #{hops_taken}"
+    IO.inspect current_map
 
     is_file_found = Enum.member?(current_map.local_file, file_name)
 
-    # IO.puts is_file_found
-    # IO.inspect current_map
+    # IO.inspect is_file_found
 
-    if not is_file_found do
-     
+    if not is_file_found do 
+      # Send request to the closest node in the finger table which is <= key
       node_to_send_to = 
         current_map.finger_table
           |> Enum.filter(fn x -> x <= key end)
-          |> Enum.max()
+          |> Enum.min_by(fn x -> (key - x) end, fn -> nil end)
+          # |> Enum.max()
+          # |> List.last
 
+      # If no node <= key exists then send request to the successor
       node_to_send_to =
         if node_to_send_to == nil, do: Enum.at(current_map.successor, 0), else: node_to_send_to
 
-      IO.inspect node_to_send_to
-        GenServer.cast(node_to_send_to |> Integer.to_string |> String.to_atom,
+      # IO.inspect node_to_send_to
+      GenServer.cast(node_to_send_to |> Integer.to_string |> String.to_atom,
         {:search, [file_name, key, hops_taken + 1]})
-    
+
     else
       IO.puts("File found in #{hops_taken} hops")
-        
     end
+
     {:noreply, current_map}
   end
 end
